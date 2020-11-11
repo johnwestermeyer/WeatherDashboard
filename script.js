998c4b361664eeef6267511d038960b5d26cdff7
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-11-10 17:34:42 -0800

    added 5 day forecast population

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let APIKey = "8375474e12a3c07e327029469afe5cd7";
-let city = "Long Beach, CA, USA";
+let city = "Long Beach, California";
 let weatherDiv = $(".weatherInfo");
 let navDiv = $(".list-group-flush");
-let fiveDay =$(".fiveDay");
+let fiveDay =$(".card-group");
 let lat, lon = "";
 
 $(document).ready(function(){
@@ -24,7 +24,7 @@ $(document).ready(function(){
         lat = response.coord.lat;
         lon = response.coord.lon;
         weatherDiv.append(`<h1 class="mt-4">${response.name} (${time})<img src="http://openweathermap.org/img/w/${response.weather[0].icon}.png"></h1>`);
-        weatherDiv.append(`<p>Temperature: ${response.main.temp} °F</p><p>Humidity: ${response.main.humidity}</p><p>Wind Speed: ${response.wind.speed} MPH</p>`)
+        weatherDiv.append(`<p>Temperature: ${response.main.temp}°F</p><p>Humidity: ${response.main.humidity}</p><p>Wind Speed: ${response.wind.speed} MPH</p>`)
         navDiv.append(`<a href="#" class="list-group-item list-group-item-action bg-light" data-city="${response.name}">${response.name}</a>`);
         uvIndex();
         historic();
@@ -49,14 +49,23 @@ function uvIndex(){
 })}
 
 function historic(){
-  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial"
+  var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&cnt=6&appid="+ APIKey + "&units=imperial";
   $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
   console.log(response);
-  fiveDay.append(`<h2 class="mt-4">5-Day Forecast:</h2>`);
-  
+  for(let i = 1; i < 6; i++){
+    var histDate = moment.unix(response.daily[i].dt).format("MM/DD/YYYY");
+    fiveDay.append(`<div class="card text-white bg-primary mr-2" style="max-width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${histDate}</h5>
+      <img src="http://openweathermap.org/img/w/${response.daily[i].weather[0].icon}.png">
+      <p class="card-text">Temp: ${response.daily[i].temp.day}°F</p>
+      <p class="card-text">Humidity: ${response.daily[i].humidity}</p>
+    </div>
+  </div>`)
+  }
 })
 }
   
